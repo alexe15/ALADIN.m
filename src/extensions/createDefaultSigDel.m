01977d39581dd2de38961e2bec9f80677dc9a29d
alexander.engelmann <alexander.engelmann@kit.edu>
2020-02-27 17:27:26 +0100

    added missing files

---
+++ src/extensions/createDefaultSigDel.m
@@ -0,0 +1,12 @@
+function [SSig , Del] = createDefaultSigDel( sProb )
+% compute default \Sigma and \Delta  matrices. 
+NsubSys = length(sProb.zz0);
+
+for i=1:NsubSys
+    SSig{i} = eye(size(sProb.zz0{i},1));
+end
+
+Del = eye(size(sProb.AA{1},1));
+
+end
+
