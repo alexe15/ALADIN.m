552ad6deb855b856b9386d88e9b157820c3a4288
alexander.engelmann <alexander.engelmann@kit.edu>
2020-03-04 15:31:48 +0100

    added test for run_ADMMnew

--- test/testADMM.m
+++ test/testADMM.m
@@ -1,13 +1,27 @@
 
-%% run with ADMM        
+%% test ADMM     
+% test BFGS for OPF 30 bus
+load('./problem_data/IEEE30busPrbFrm.mat')
 
 
-semilogy(vecnorm([sProb.AA{:}]*sol_ADMM.logg.X))
+% bring into the correct foormat
+sProb.locFuns.ffi = ffifun;
+sProb.locFuns.ggi = ggifun;
+sProb.locFuns.hhi = hhifun;
+sProb.AA          = dOPF.AA;
+sProb.zz0         = dOPF.xx0;
+sProb.lam0        = 0.01*ones(Ncons,1); 
+sProb.llbx        = dOPF.lbx;
+sProb.uubx        = dOPF.ubx;
 
-% use a few ADMM iteration as "heuristic globalization"
-optsADM    = struct('rho',1e5,'scaling',false,'rhoUpdate',false,'maxIter',50);
+% solve with ADMM
+optsADM    = struct('rho',1e4,'scaling',false,'rhoUpdate',false,'maxIter',400);
 sol_ADMM   = run_ADMMnew(sProb, optsADM);
 
-sProb.zz0  = sol_ADMM.xxOpt;
-% use everage of assigned Lagrange multipliers for initialization
-sProb.lam0 = (sol_ADMM.lamOpt{1} + sol_ADMM.lamOpt{2} + sol_ADMM.lamOpt{3} + sol_ADMM.lamOpt{4})/2;
+% centralized solution
+res_IPOPT  = run_IPOPT(sProb);
+
+% check whether primal solution is close enough to centralized one
+assert(norm(vertcat(sol_ADMM.xxOpt{:}) - res_IPOPT.x,inf) < 1e-2, 'Out of tolerance for local minimizer!')
+    
+% semilogy(vecnorm([sProb.AA{:}]*sol_ADMM.logg.X))
