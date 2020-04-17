48c2c43e24e2dcea9c9994ae2639ab32edec182f
Tillmann Mühlpfordt <tillmann.muehlpfordt@kit.edu>
2020-04-17 11:43:56 +0200

    code runs now without dependencies on locFunsCas, xxCas; tested only for morenet example! #84

--- src/core/createLocSolAndSens.m
+++ src/core/createLocSolAndSens.m
@@ -2,14 +2,13 @@ function [ sProb ] = createLocSolAndSens(sProb, opts)
 % set up the local solvers and sensitivity functions for coord. QP
 
 % convert MATLAB functions to CasADi functions 
-if ~isfield(sProb, 'p')
-    [ sProb.locFunsCas, sProb.xxCas ]             = mFun2casFun(sProb, opts);
-else
-    [ sProb.locFunsCas, sProb.xxCas, sProb.pCas ] = mFun2casFun(sProb, opts);
+if ~isfield(sProb, 'sens')
+    if ~isfield(sProb, 'p')
+        [ sProb.locFunsCas, sProb.xxCas ]             = mFun2casFun(sProb, opts);
+    else
+        [ sProb.locFunsCas, sProb.xxCas, sProb.pCas ] = mFun2casFun(sProb, opts);
+    end
 end
-
-% create local solvers 
-% [ sProb.nnlp, sProb.gBounds, sProb.rhoCas, sProb.lamCas, sProb.SSig ] = createLocalSolvers(sProb, opts);
 
 % compute sensitivities (gradient, Jacobian, ...)
 sProb.rhoCas = opts.sym('rho',1,1);
@@ -21,13 +20,10 @@ else
     fprintf('Using given sensitivities...\n');
 end
 pause(1)
-
 [ sProb.nnlp, sProb.gBounds, sProb.rhoCas] = createLocalSolvers(sProb, opts);
-
-
                              
 % set up a merit function for line search
-sProb.Mfun          = createMfun(sProb, opts);
+% sProb.Mfun          = createMfun(sProb, opts);
 
 % optional functions
 if opts.lamInit == true
