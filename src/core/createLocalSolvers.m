0835b40eb0ba59a0355caf4390d1b855d6391e4c
Xinliang <uthgg@student.kit.edu>
2020-05-22 21:11:15 +0200

    worhp prototype, LS-problem, fixes #107

--- src/core/createLocalSolvers.m
+++ src/core/createLocalSolvers.m
@@ -28,7 +28,7 @@ for i=1:NsubSys
         pars = [];
         funs = sProb.locFuns;
         sens = sProb.sens;
-        solve_nlp = @(x, z, rho, lambda, Sigma, pars)build_local_NLP(funs.ffi{i}, funs.ggi{i}, funs.hhi{i}, sProb.AA{i}, lambda, rho, z, Sigma, x, sProb.llbx{i}, sProb.uubx{i}, sens.JJac{i}, sens.gg{i}, sens.HH{i});   
+        solve_nlp = @(x, z, rho, lambda, Sigma, pars)build_local_NLP_with_fmincon(funs.ffi{i}, funs.ggi{i}, funs.hhi{i}, sProb.AA{i}, lambda, rho, z, Sigma, x, sProb.llbx{i}, sProb.uubx{i}, sens.JJac{i}, sens.gg{i}, sens.HH{i});   
     elseif strcmp(solver, 'Casadi+Ipopt')
         assert(isfield(sProb, 'locFunsCas'), 'locFunsCas field is missing')
         nlp_reference = build_nlp_reference(sProb.xxCas{i},...
@@ -40,10 +40,16 @@ for i=1:NsubSys
                                             rhoCas,...
                                             opts);
         pars = struct('lam_g0', [], 'lam_x0', []);
-        solve_nlp  = @(x, z, rho, lambda, Sigma, pars)build_nlp_with_casadi(x, z, rho, lambda, Sigma, pars, nlp_reference, sProb.llbx{i}, sProb.uubx{i}, gBounds.llb{i}, gBounds.uub{i});
-    end    
+        solve_nlp  = @(x, z, rho, lambda, Sigma, pars)build_nlp_with_casadi(funs.ffi{i}, funs.ggi{i}, funs.hhi{i}, sProb.AA{i}, lambda, rho, z, Sigma, x, sProb.llbx{i}, sProb.uubx{i}, sens.JJac{i}, sens.gg{i}, sens.HH{i});
+   
+    elseif strcmp(solver, 'worhp')
+        pars = [];
+        funs = sProb.locFuns;
+        sens = sProb.sens;
+        solve_nlp = @(x, z, rho, lambda, Sigma, pars)build_local_NLP_with_worhp(funs.ffi{i}, funs.ggi{i}, funs.hhi{i}, sProb.AA{i}, lambda, rho, z, Sigma, x, sProb.llbx{i}, sProb.uubx{i}, sens.JJac{i}, sens.gg{i}, sens.HH{i});
+    end
     
-    nnlp{i} = struct('solve_nlp', solve_nlp, 'pars', pars); 
+nnlp{i} = struct('solve_nlp', solve_nlp, 'pars', pars); 
 end
 end
 
@@ -79,7 +85,7 @@ function res = build_nlp_with_casadi(x, z, rho, lambda, Sigma, pars, nlp, lbx, u
     res.pars.lam_x = res.lam_x;
 end
 
-function res = build_local_NLP(f, g, h, A, lambda, rho, z, Sigma, x0, lbx, ubx, dgdx, dfdx, Hessian)
+function res = build_local_NLP_with_fmincon(f, g, h, A, lambda, rho, z, Sigma, x0, lbx, ubx, dgdx, dfdx, Hessian)
     opts = optimoptions('fmincon');
     opts.Algorithm = 'interior-point';
     opts.CheckGradients = false;
@@ -99,12 +105,25 @@ function res = build_local_NLP(f, g, h, A, lambda, rho, z, Sigma, x0, lbx, ubx, 
             opts.HessianApproximation = 'lbfgs';
         end
     end
-    cost = @(x)build_cost_function(x, f(x), dfdx(x), lambda, A, rho, z, Sigma);
+    cost    = @(x)build_cost_function(x, f(x), dfdx(x), lambda, A, rho, z, Sigma);
     nonlcon = @(x)build_nonlcon(x, g, h, dgdx);
     [xopt, fval, flag, out, multiplier] = fmincon(cost, x0, [], [], [], [], lbx, ubx, nonlcon, opts);
     res.x = xopt;
-    res.lam_g = [multiplier.eqnonlin; multiplier.ineqnonlin];
+    res.lam_g =[];% [multiplier.eqnonlin; multiplier.ineqnonlin];
     res.lam_x = max(multiplier.lower, multiplier.upper);
+    res.pars = [];
+end
+
+function res = build_local_NLP_with_worhp(f, g, h, A, lambda, rho, z, Sigma, x0, lbx, ubx, dgdx, dfdx, Hessian)
+    % assumption: least-square problem, i.e. without constraints
+    cost = @(x)build_cost(x, f(x), lambda, A, rho, z, Sigma);
+    grad = @(x)build_grad(x, dfdx(x), lambda, A, rho, z, Sigma);
+    % unconstrained problem and Hessian is computed by hand
+    Hess = @(x, mu, scale)(scale * Hessian(x,lambda) + scale * rho*Sigma);
+    [xopt, multiplier] = worhp_interface(cost,grad,[],[],Hess,x0',[],lbx',ubx',[],[]);
+    res.x = xopt;
+    res.lam_g = [];
+    res.lam_x = multiplier;
     res.pars = [];
 end
 
@@ -112,10 +131,18 @@ function [fun, grad] = build_cost_function(x, f, dfdx, lambda, A, rho, z, Sigma)
 	%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     % the code assumes that Sigma is symmetric and positive definite!!!
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    fun = f + lambda'*A*x + 0.5*rho*(x - z)'*Sigma*(x - z);
+    fun = build_cost(x, f, lambda, A, rho, z, Sigma);
     if nargout > 1
-        grad = dfdx + A'*lambda + rho*Sigma*(x - z);
+        grad = build_grad(x, dfdx, lambda, A, rho, z, Sigma);
     end
+end
+
+function fun = build_cost(x, f, lambda, A, rho, z, Sigma)
+    fun = f + lambda'*A*x + 0.5*rho*(x - z)'*Sigma*(x - z);
+end
+
+function grad = build_grad(x, dfdx, lambda, A, rho, z, Sigma)
+    grad = dfdx + A'*lambda + rho*Sigma*(x - z);
 end
 
 function [ineq, eq, jac_ineq, jac_eq] = build_nonlcon(x, g, h, dgdx)
